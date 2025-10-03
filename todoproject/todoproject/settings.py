ac647cb1f9f1ebb865880d33ca030df72c8a3b71
pinchase <kagiripeterson8404@gmail.com>
2025-10-03 15:46:54 +0300

    fixed static files rendering

--- todoproject/todoproject/settings.py
+++ todoproject/todoproject/settings.py
@@ -98,7 +98,7 @@ USE_TZ = True
 
 # Static files configuration
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Security settings for production
