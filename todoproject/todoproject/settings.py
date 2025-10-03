412bbc6b6b77500691aec23d08485786458d4b44
pinchase <kagiripeterson8404@gmail.com>
2025-10-03 15:44:31 +0300

    fixed allowed hostts for render

--- todoproject/todoproject/settings.py
+++ todoproject/todoproject/settings.py
@@ -7,7 +7,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "todo-wj10.onrender.com",
+]
 
 # Application definition
 INSTALLED_APPS = [
